a80e7f62b3d63bcc042bd5be2f06b1d2b5709a83
unknown <alpa.gupta@gmail.com>
2019-07-16 10:31:57 -0400

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EquitiesProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StockContribution_Click(object sender, RoutedEventArgs e)
+        {
+            StockContribution StockContributionObject = new StockContribution();
+            StockContributionObject.Show();
+        }
+
+        private void Allocation_Click(object sender, RoutedEventArgs e)
+        {
+            DiversifiedPortfolio DiversifiedPortfolioObject = new DiversifiedPortfolio();
+            DiversifiedPortfolioObject.Show();
+        }
+
+        private void StockContribution1_Click(object sender, RoutedEventArgs e)
+        {
+            StockContributionRateofReturn StockContributionRateofReturnObject = new StockContributionRateofReturn();
+            StockContributionRateofReturnObject.Show();
+        }
+    }
+}
---
+++ StockContribution.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EquitiesProject
+{
+    /// <summary>
+    /// Interaction logic for StockContribution.xaml
+    /// </summary>
+    public partial class StockContribution : Window
+    {
+        public StockContribution()
+        {
+            InitializeComponent();
+        }
+    }
+}
